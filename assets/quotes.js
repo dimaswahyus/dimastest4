c5623e397298c541aefdfbb3bfb1a96361473d85
dimaswahyus <64754947+dimaswahyus@users.noreply.github.com>
2020-05-04 06:36:17 +0700

    Initial commit

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "DimasWahyus", quotes: "Whatever happens, keep breathing" },
+    { author: "DimasWahyus", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "DimasWahyus", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "DimasWahyus", quotes: "I love you." },
+    { author: "DimasWahyus", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "DimasWahyus", quotes: "I love you." },
+    { author: "DimasWahyus", quotes: "I'm sorry for loving you." },
+];
